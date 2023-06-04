55508fdcdcf65e0e7034da2dda8957ff6442b842
Bexultan Kuralbay <30636@iitu.edu.kz>
2023-06-04 10:18:45 +0600

    i add functions

--- src/main/java/com/example/trello/controllers/HomeController.java
+++ src/main/java/com/example/trello/controllers/HomeController.java
@@ -5,9 +5,12 @@ import com.example.trello.entities.Folders;
 import com.example.trello.entities.TaskCategories;
 import com.example.trello.services.FolderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -17,19 +20,16 @@ import java.util.List;
 public class HomeController {
 
     private final FolderService folderService;
-    @GetMapping
+    @GetMapping(value = "/")
     public String main(Model model){
         List<Folders> folders = folderService.getAllFolder();
         model.addAttribute("folders", folders);
         return "main-page";
     }
-    @GetMapping
-    public String folder_detail(Model model, @RequestParam(name = "id") Long id){
+    @GetMapping(value = "detail-folder/{folder_id}")
+    public String detail_folder(@PathVariable(name = "folder_id") Long id, Model model){
         Folders folder = folderService.getFolderById(id);
         model.addAttribute("folder",folder);
         return "folderDetails";
-    }
-    public String addCategory(Model model, TaskCategories taskCategories){
-        return "redirect:/";
     }
 }
